4fef31a86cea69c248fd9bbba6ddcbc1dcc1e4b3
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-07 22:36:52 -0700

    Tweaking test name.

--- test_yabai_navigator.py
+++ test_yabai_navigator.py
@@ -2,7 +2,7 @@ from yabai_navigator import YabaiNavigator
 from unittest.mock import Mock
 
 
-def test_focus_on_stacked_window():
+def test_focus_on_stacked_window_passes_in_window_id():
     mock = Mock()
     test_window_id = "test_window_id"
 
