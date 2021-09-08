832c367c667c91191781341abaf8bcb3d94941aa
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-08 11:39:17 -0700

    Simplifying YabaiProvider's call_yabai.

--- yabai_provider.py
+++ yabai_provider.py
@@ -1,17 +1,15 @@
 import json
 import subprocess
+from typing import Any
 
 
 # TODO: Include "yabai" as an already included argument
 class YabaiProvider:
     """Class to abstract out calling out to Yabai"""
-    def call_yabai(self, args, return_data=True):
+    def call_yabai(self, args) -> Any:
         if args[0] != "yabai":
             args = ["yabai"] + args
 
-        if return_data:
-            return json.loads(
-                subprocess.run(args,
-                               stdout=subprocess.PIPE).stdout.decode('utf-8'))
-        else:
-            subprocess.run(args, stdout=subprocess.PIPE).stdout.decode('utf-8')
+        return json.loads(
+            subprocess.run(args,
+                           stdout=subprocess.PIPE).stdout.decode('utf-8'))
