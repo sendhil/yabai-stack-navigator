f8727e372b04bde34714badee9f74a87e21d1c86
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-07 23:09:32 -0700

    Removing unused method.

--- yabai_layout_details.py
+++ yabai_layout_details.py
@@ -20,12 +20,6 @@ class YabaiLayoutDetails:
     def get_layout_index(self, layout_data):
         return layout_data["index"]
 
-    # TODO: Remove as this might not be used
-    def get_focused_window(self, window_data):
-        for window in window_data:
-            if window["focused"] == 1:
-                return window
-
     def sort_stacked_windows(self, window_data):
         sorted_window_data = sorted(window_data,
                                     key=lambda i: i['stack-index'])
