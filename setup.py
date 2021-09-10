9040db07a09cdd3a41c6704770b09f6a50d6765b
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-09 23:22:57 -0700

    Shrinking long description + bumping package version

--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(name='yabai_stack_navigator',
-      packages=find_packages(),
-      version='1.0.0',
-      description='Script to make navigating between stacks on Yabai easier.',
-      author='Sendhil Panchadsaram',
-      license='MIT',
-      long_description=long_description,
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      test_suite='tests',
-      py_modules=['main', 'yabai_stack_navigator'],
-      project_urls={
-          'GitHub': 'https://github.com/sendhil/yabai-stack-navigator',
-      },
-      entry_points='''
+setup(
+    name='yabai_stack_navigator',
+    packages=find_packages(),
+    version='1.0.0',
+    description='Script to make navigating between stacks on Yabai easier.',
+    author='Sendhil Panchadsaram',
+    license='MIT',
+    long_description=
+    "A simple script to make navigating between stacks and windows in Yabai(https://github.com/koekeishiya/yabai) easier.",
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    py_modules=['main', 'yabai_stack_navigator'],
+    project_urls={
+        'GitHub': 'https://github.com/sendhil/yabai-stack-navigator',
+    },
+    entry_points='''
         [console_scripts]
         yabai-stack-navigator=main:main
     '''),
