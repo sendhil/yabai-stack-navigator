00e089e62a377a1284765992d0ef091f5512f91a
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-09 23:25:11 -0700

    Another setup.py tweak + version bump

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='yabai_stack_navigator',
     packages=find_packages(),
-    version='1.0.1',
+    version='1.0.2',
     description='Script to make navigating between stacks on Yabai easier.',
     author='Sendhil Panchadsaram',
     license='MIT',
     long_description=
-    "A simple script to make navigating between stacks and windows in Yabai(https://github.com/koekeishiya/yabai) easier.",
+    "A simple script to make navigating between stacks and windows in Yabai(https://github.com/koekeishiya/yabai) easier. Details at https://github.com/sendhil/yabai-stack-navigator.",
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
